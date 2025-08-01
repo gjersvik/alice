f2334958f4f1cf84910f162201fc3c5773e1fb16
Ole Martin Gjersvik <olgj@equinor.com>
2025-08-01 09:01:58 +0000

    Added Web Awesome to the project.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+import '@awesome.me/webawesome/dist/styles/webawesome.css';
+import '@awesome.me/webawesome/dist/components/button/button.js';
+
 async function main() {
-    console.log("Hello world!")
+    const button = document.getElementById('hello');
+    button?.addEventListener('click', e => {
+        console.log('Hello world.')
+    })
 }
 
 main().catch( e => console.log("Main expesption:", e) );
