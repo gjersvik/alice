11a9c097b549f522ef244c64be38f969dd1beaa9
Ole Martin Gjersvik <olgj@equinor.com>
2025-08-01 10:22:10 +0000

    Play around with WebLLM to test model loading.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
 import '@awesome.me/webawesome/dist/styles/webawesome.css';
 import '@awesome.me/webawesome/dist/components/button/button.js';
 
+import * as webllm from '@mlc-ai/web-llm';
+import { CreateMLCEngine } from '@mlc-ai/web-llm';
+
+const model = 'SmolLM2-360M-Instruct-q4f16_1-MLC';
+
 async function main() {
-    const button = document.getElementById('hello');
-    button?.addEventListener('click', e => {
-        console.log('Hello world.')
+    const list = document.getElementById('list');
+    list?.addEventListener('click', e => {
+        console.log(webllm.prebuiltAppConfig.model_list)
+    })
+
+    const load = document.getElementById('load');
+    load?.addEventListener('click', async e => {
+        let inCache = await webllm.hasModelInCache(model);
+        console.log("Is SmolLM2-360M-Instruct-q4f16_1-MLC in cache?", inCache);
+
+        const initProgressCallback = (progress) => {
+            console.log("Model loading progress:", progress);
+        };
+        const engine = await CreateMLCEngine(model, { initProgressCallback });
+        inCache = await webllm.hasModelInCache(model);
+        console.log("Is SmolLM2-360M-Instruct-q4f16_1-MLC in cache?", inCache);
+    })
+
+    const unload = document.getElementById('unload');
+    unload?.addEventListener('click', async e => {
+        await webllm.deleteModelInCache(model)
+        console.log(`Removed: ${model} i hope.`);
     })
 }
 
